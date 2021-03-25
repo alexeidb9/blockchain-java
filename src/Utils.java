c25c924c0ab2fda0e74e4893bae1aae6d79147b1
Alexei A <alexey.alexandrenko@gmail.com>
2021-03-25 08:04:19 +0100

    Class Utils & zeros method were added

--- src/Utils.java
+++ src/Utils.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Utils {
+
+    public String zeros(int length) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            builder.append('0');
+        }
+        return.builder.toString();
+    }
+
+
 }
